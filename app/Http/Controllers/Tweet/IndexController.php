4371a733a8b0dba8e03765a5de861d568b438a0d
potem3190 <casvalazm@gmail.com>
2026-02-17 07:41:02 +0900

    サービスクラスにつぶやきを全件取得する処理を抜き出し

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -5,15 +5,16 @@ namespace App\Http\Controllers\Tweet;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\models\Tweet;
+use App\Services\TweetService;
 
 class IndexController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
+        $tweets = $tweetService->getTweets();
         return view('tweet.index')->with('tweets', $tweets);
     }
 }
